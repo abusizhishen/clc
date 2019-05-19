4db220484d709b8310ae7f04f72a2f9aea77cbf3
wanghongwei <abusizhishen@gmail.com>
2019-05-19 09:38:37 +0800

    人员列表中添加领队

--- signList.py
+++ signList.py
@@ -2,6 +2,7 @@ import os, sys
 from requests import Session
 from lxml import html
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
 
 url = "http://www.clchuwai.com/travel/detail/%s#userjoin"
 def ciyun(text, id):
@@ -29,22 +30,55 @@ def get_text(id):
     if resp.status_code == 302:
         exit("活动%s不存在" % id)
 
-    tree = html.fromstring(resp.text)
+    tree = html.fromstring(resp.text, )
     node = tree.xpath("//ul[@class=\"list\"]//p")
     users = []
     for i in node:
         users.append(i.text)
 
+    dom = BeautifulSoup(resp.text, 'lxml')
+    div = dom.find_all("div", {"class": "sub-detail"})[1]
+    content = div.text
+
+    for i in find_leader(content, "【 领 队 】"):
+        users.append(i)
+    for i in find_leader(content, "【副领队】"):
+        users.append(i)
+
     return users
+
+
+def find_leader(content, type):
+    arr = content.split(type)
+    users = []
+    if len(arr) > 1:
+        for i in range(len(arr)):
+            str = arr[i]
+            find1 = str.find(",")
+            find2 = str.find("，")
+
+            l = []
+            if find1 != -1:
+                l.append(find1)
+            if find2 != -1:
+                l.append(find2)
+
+            leader = str[:l[0]]
+            users.append(leader)
+
+    return users
+
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) <2 :
+    if len(args) < 2:
         exit("请输入活动id")
 
     id = args[1]
-    print("活动链接地址：" , url % id)
+    print("活动链接地址：", url % id)
     users = get_text(id)
-    if len(users) == 0 :
+    if len(users) == 0:
         exit("没有报名人员")
-    ciyun(" ".join(users),id)
+    else:
+        print("总人数：", len(users))
+    ciyun(" ".join(users), id)
